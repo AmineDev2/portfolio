cfa921e2d9971a2fa9fc938bc58379953452b016
AmineDev2 <djatouf.amine@gmail.com>
2026-01-10 15:54:24 +0100

    rr

--- src/app/qualite/qualite.component.ts
+++ src/app/qualite/qualite.component.ts
@@ -10,36 +10,41 @@ export class QualiteComponent {
     {
       nom: 'Affiche : Ici on cuisine Bio',
       src: 'assets/affiche-bio-occitanie.png',
+      source: 'Affiche éditée par la Région Occitanie',
       ouvert: false,
     },
     {
       nom: 'Affiche : Produits locaux et régionaux',
       src: 'assets/affiche-produits-occitanie.png',
+      source: 'Affiche éditée par la Région Occitanie',
       ouvert: false,
     },
     {
       nom: "Affiche : Label pour les cantines d'Occitanie",
       src: 'assets/affiche-label-occitanie.png',
+      source: 'Affiche éditée par la Région Occitanie',
       ouvert: false,
     },
     {
       nom: 'Affiche : Pain bio et local',
       src: 'assets/affiche-pain-bio.png',
+      source: 'Affiche éditée par le lycée Charlemagne',
       ouvert: false,
     },
     {
       nom: 'Affiche : Lutte contre le gaspillage',
       src: 'assets/affiche-faim-gaspillage.png',
+      source: 'Affiche éditée par le Campus Terre et Nature',
       ouvert: false,
     },
     {
       nom: "Sticker : Niveau 2 Cant'Occ",
       src: 'assets/sticker-cantocc.png',
+      source: 'Affiche éditée par la Région Occitanie',
       ouvert: false,
     },
   ];
 
-  // Fonction pour basculer l'état ouvert/fermé
   toggleDoc(doc: any) {
     doc.ouvert = !doc.ouvert;
   }
